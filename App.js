344d1504444cb9d3cf1815612b01de5280ad6b85
yesterdaykite <yesterdaykite@yesterdaykites-MacBook-Pro.local>
2020-06-01 21:48:15 +0900

    #3 Coding the UI part One

--- App.js
+++ App.js
@@ -1,10 +1,16 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, StatusBar, TextInput, Dimensions,Platform } from 'react-native';
 
+
+const {height, width} = Dimensions.get("window");
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <StatusBar barStyle= "light-content"/>
+      <Text style = {styles.title}>yesterday🪁TODO</Text>
+      <View style = {styles.card}>
+        <TextInput style = {styles.input} placeholder = {"new To do"} />
+      </View>
     </View>
   );
 }
@@ -12,8 +18,36 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#474787',
     alignItems: 'center',
     justifyContent: 'center',
   },
+  title : {
+    color : "white",
+    fontSize : 30,
+    marginTop : 50,
+    fontWeight : "200",
+    marginBottom : 30
+  },
+  card : {
+    backgroundColor : "white",
+    flex : 1,
+    width : width - 25,
+    borderTopLeftRadius : 10,
+    borderTopRightRadius : 10,
+    ...Platform.select({
+      ios : {
+        shadowColor : "rgba(50,50,50)",
+        shadowOpacity : 0.5,
+        shadowRadius : 5,
+        shadowOffset : {
+          height : -1,
+          width : 0
+        }
+      },
+      android : {
+        elevation : 3
+      }
+    })
+  }
 });
